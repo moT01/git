5c3f71dc0707e4e87e3c792d16afba1443ab8900
moT01 <20648924+moT01@users.noreply.github.com>
2020-07-14 13:22:52 -0500

    50.1

---
+++ freeCodeCamp/test/50.test.js
@@ -0,0 +1,14 @@
+const assert = require('assert');
+const { getLastCommand, getCwd } = require('./utils');
+
+describe('tests', () => {
+  let cwd, lastCommand, websiteDirectory;
+  before(async () => {
+    lastCommand = await getLastCommand();
+    cwd = await getCwd();
+  });
+
+  it('should pass', async () => {
+    assert(lastCommand[0] === 'git' && lastCommand[1] === 'status' && cwd === '/home/strove/project/website');
+  });
+});
